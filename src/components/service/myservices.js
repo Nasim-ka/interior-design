56b9e5da2e8d416f77daebd1413a4244c639dcf4
nasim ka <nasimka69@gmail.com>
2024-08-06 04:27:03 -0700

    deploy interior design project

---
+++ src/components/service/myservices.js
@@ -0,0 +1,41 @@
+import furniture from "../../../public/images/SERVICES/furniture.jpg"
+import outdoor from "../../../public/images/SERVICES/outdoor.jpg"
+import lighting from "../../../public/images/SERVICES/lighting.jpg"
+import  sofas from "../../../public/images/SERVICES/sofas.jpg"
+import accessories from "../../../public/images/SERVICES/accessories.jpg"
+import kitchenaccessories from "../../../public/images/SERVICES/kitchenaccessories.jpg"
+const myservices=[
+    {
+        id:1,
+        title:"Furniture",
+        imgURL:furniture,
+    },
+    {
+        id:2,
+        title:"Outdoor",
+        imgURL:outdoor,
+    },
+   
+    {
+        id:3,
+        title:"Lighting",
+        imgURL:lighting,
+    },
+    {
+        id:4,
+        title:"Sofas & Armchairs",
+        imgURL:sofas,
+    },
+    {
+        id:5,
+        title:"Accessories",
+        imgURL:accessories,
+    },
+    {
+        id:6,
+        title:"Kitchen Accessories",
+        imgURL:kitchenaccessories,
+    },
+    
+]
+export default myservices
